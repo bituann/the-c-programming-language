d93dc05f649e21dd58ae05ea2dc6ec25f5ad476c
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-09 19:29:10 +0100

    Set up program
    Functions declaration, symboloc constant definitions, and initial algorithm specification

---
+++ chapter-1/exercise1_20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+#define MAXLINE 1000
+#define TABSTOP 8
+
+
+void detab (char string[], int len, int tabstop);
+void shiftArrayItems (char array[], int len, int start, int shift);
+void addChar (char line[], )
+int getCurrentLine (char line[], int limit);
+
+
+int main ()
+{
+	
+}
+
+
+int detab (char s[], int len, int tabstop)
+{
+	//check for tab in the line
+	// if tab is found
+		//determine how many columns left till tabstop
+		//make space for the blank characters by shifing the remaining characters
+		//add that many blank characters
+}
