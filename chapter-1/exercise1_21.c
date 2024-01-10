41aa90c8004ce616b9d465a4e0ebf148a120c160
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-10 12:44:09 +0100

    Program Setup
    Do the following to kickstart program development
    - Declare functions
    - Define symbolic comstants
    - Write algorithm for lrogram to follow

---
+++ chapter-1/exercise1_21.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+#define MAXLINE 1000
+#define TABSTOP 8
+
+
+void entab (char string[], int length, int tabstop);
+int countBlanks (char line[], int start);
+void putChar (char character, char string[], int index);
+void removeArrayItem (char array[], int index);
+int increment (int num, int increment);
+int decrement (int num, int decrement);
+int getCurrentLine (char array[], int limit);
+
+
+int main ()
+{
+	
+}
+
+
+void entab (char s[], int len, int tabstop)
+{
+	//search through line
+	//if space is encountered, count how many cosequtive blanks exists
+	//if blanks > 1
+	//determine how many spaces left till tabstop
+	//if it's > blanks, add spaces
+	//if it's <= blanks, add tabs until it's > blank
+	//remove extra spacs in string
+	//increment i, colCounter as necessary
+}
