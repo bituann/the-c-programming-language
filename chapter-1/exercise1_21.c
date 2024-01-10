67108fa2ab62959d7e53bbc6dd821093c72293ab
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-10 21:52:03 +0100

    Define getCurrentLine function

--- chapter-1/exercise1_21.c
+++ chapter-1/exercise1_21.c
@@ -70,3 +70,20 @@ int decrement (int num, int decrement)
 {
 	return num -= decrement;
 }
+
+int getCurrentLine (char s[], int lim)
+{
+	int i, c;
+	
+	for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != '}'; ++i)
+		s[i] = c;
+		
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	
+	s[i] = '\0';
+	
+	return i;
+}
