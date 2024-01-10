cb16bfbe35df121e1ca7eabe93992963cbf3088a
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-10 23:12:21 +0100

    Adjust removeArrayItem function

--- chapter-1/exercise1_21.c
+++ chapter-1/exercise1_21.c
@@ -100,10 +100,12 @@ void putChar (char character, char s[], int index)
 
 void removeArrayItem (char a[], int index)
 {
-	for (int i = index; a[i]; ++i)
+	int i = index;
+	
+	for (i = index; a[i]; ++i)
 		a[i] = a[i + 1];
 	
-	a[index] = a[index + 1];
+	a[i] = a[i + 1];
 }
 
 
