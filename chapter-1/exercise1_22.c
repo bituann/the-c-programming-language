933c928189bc9656c8b86a9f950fda3ff7f50079
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-11 13:59:51 +0100

    Project Setup
    - Declare functions
    - Define symbolic constants
    - Write algorithm for program

---
+++ chapter-1/exercise1_22.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+#define MAXLINE 1000
+#define MAXCOL 10
+
+
+void fold (char line[], int len);
+void putChar (char character, char array[], int index);
+void shiftArrayItems (char array[], int length);
+int increment (int num, int increment);
+int decrement (int num, int decrement);
+int getCurrentLine (char array[], int limit);
+
+
+int main
+{
+	
+}
+
+
+void fold (char s[], int len)
+{
+	//loop through array
+	//keep track of current col, and index of last non-blank char
+	//when maxcol is reached
+	//shift everything from just after last non-blank char to the right
+	//insert new line char
+}
