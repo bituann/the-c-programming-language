2dea236cb6c7801e11bf5c3dea52cbc8dde76258
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-01-12 16:20:49 +0100

    Program setup
    - Define symbolic constants
    - Declare functions
    - Write algorithm for checkBasicSyntaxErrors function

---
+++ chapter-1/exercise1_24.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+#define MAXLINE 100000
+
+
+void checkBasicSyntaxErrors (char text[]);
+int checkSingleQuotes (char text[]);
+int checkDoubleQuotes (char text[]);
+int checkCurlyBraces (char text[]);
+int checkParentheses (char text[]);
+int checkBrackets (char text[]);
+int countCommentChars (char text[], int start);
+int increment (int num, int increment);
+int decrement (int num, int decrement);
+int getCurrentLine (char array[], int limit);
+
+
+int main ()
+{
+	
+}
+
+void checkBasicSyntaxErrors (char t[])
+{
+	//initialize variables
+	//check for single quotes
+	//print message
+	//check for double quotes
+	//lrint message
+	//check for curly braces
+	//print message
+	//check for parentheses
+	//print message
+	//check for bracket
+	//print message
+}
